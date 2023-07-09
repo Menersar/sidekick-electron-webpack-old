54bf584599ca10f30ec43ff17cbae1d7f7951764
Menersar <menersar@menersar.com>
2023-07-09 16:18:14 +0200

    add sidekick support

--- src/main.ts
+++ src/main.ts
@@ -293,7 +293,7 @@ export class WebpackConfigurator {
     if (this.type === "main") {
       externals.push("webpack/hot/log-apply-result")
       // externals.push("electron-webpack/out/electron-main-hmr/HmrClient")
-      externals.push("electron-webpack/out/electron-main-hmr/HmrClient")
+      externals.push("sidekick-electron-webpack/out/electron-main-hmr/HmrClient")
       externals.push("source-map-support/source-map-support.js")
     }
 
